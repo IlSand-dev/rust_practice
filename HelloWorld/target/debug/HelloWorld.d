7cc4a85a22f37a85434b563ad89aaad74aeaaa7f
IlyaCoolDev <ilya@d3en.ru>
2023-06-27 13:52:24 +0300

    guessing game first version

---
+++ HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+/home/ilya/projects/rust_practice/HelloWorld/target/debug/HelloWorld: /home/ilya/projects/rust_practice/HelloWorld/src/main.rs
---
+++ guessing_game/target/debug/build/libc-5ccc621efa3fda13/build_script_build-5ccc621efa3fda13.d
@@ -0,0 +1,5 @@
+/home/ilya/projects/rust_practice/guessing_game/target/debug/build/libc-5ccc621efa3fda13/build_script_build-5ccc621efa3fda13: /home/ilya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
+
+/home/ilya/projects/rust_practice/guessing_game/target/debug/build/libc-5ccc621efa3fda13/build_script_build-5ccc621efa3fda13.d: /home/ilya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs
+
+/home/ilya/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.147/build.rs:
