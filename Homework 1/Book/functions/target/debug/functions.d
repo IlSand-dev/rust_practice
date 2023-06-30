3cead0c2af0f09cd41811dc6d8db4f213059c6bf
IlyaCoolDev <ilya@d3en.ru>
2023-06-30 12:14:49 +0300

    book chapters 1-4

---
+++ Homework 1/Book/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/ilya/projects/rust_practice/Homework\ 1/Book/functions/target/debug/functions: /home/ilya/projects/rust_practice/Homework\ 1/Book/functions/src/main.rs
---
+++ Homework 1/Book/variables/target/debug/deps/variables-e87210e7ee119ba4.d
@@ -0,0 +1,5 @@
+/home/ilya/projects/rust_practice/Homework 1/Book/variables/target/debug/deps/variables-e87210e7ee119ba4: src/main.rs
+
+/home/ilya/projects/rust_practice/Homework 1/Book/variables/target/debug/deps/variables-e87210e7ee119ba4.d: src/main.rs
+
+src/main.rs:
